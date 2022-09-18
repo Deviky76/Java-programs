930cdd6603b37233ca850c3f82710b6dca708325
Deviky76 <111038886+Deviky76@users.noreply.github.com>
2022-09-18 05:44:16 +0530

    Add files via upload

---
+++ Simple.java
@@ -0,0 +1,34 @@
+package sample;
+
+public class Simple {
+
+	public static void main(String[] args) 
+	{
+	
+		int x;
+		int y;
+		x=10;
+		y=20;
+		int a=30;
+		int A=40;
+		System.out.println(a+A);
+		System.out.println(x+y);
+		String empname;
+		empname = "Richards";
+		System.out.println(empname);
+		System.out.println("Welcome");
+		System.out.println("Java");
+		
+
+	}
+
+}
+
+
+/* output 
+70
+30
+Richards
+Welcome
+Java
+*/
