383c9fca7dc3ac65b2b54d7b59db83241563e50b
Deviky76 <111038886+Deviky76@users.noreply.github.com>
2022-09-18 18:29:49 +0530

    Add files via upload

---
+++ String_Compare.java
@@ -0,0 +1,26 @@
+// Comparing the string using IF statement
+
+package java_prac;
+
+public class String_Compare {
+
+	public static void main(String[] args) 
+	{
+		
+		String expval, actval;
+		expval ="invalid credentials";
+		actval ="invalid credentials";
+		
+		if(expval==actval)
+		{
+			System.out.println("Test Pass");
+		}
+		else
+		{
+			System.out.println("Test Fail");
+			
+		}
+
+	}
+
+}
