f411bc6ad1300f9bca1cf75251d446044fc6c96c
Deviky76 <111038886+Deviky76@users.noreply.github.com>
2022-09-18 16:43:32 +0530

    Add files via upload

---
+++ String_Count.java
@@ -0,0 +1,18 @@
+//It counts no. of chars. in a string  length()
+package java_prac;
+
+public class String_Count {
+
+	public static void main(String[] args) 
+	{
+		String data = "Goo gle";
+		System.out.println(data.length());
+		
+		
+		int count = data.length();
+		System.out.println(count);
+		
+	}
+
+}
+ 
