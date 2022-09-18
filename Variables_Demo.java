b976a58b5b2dd0716a5d41a819d38b61f175375a
Deviky76 <111038886+Deviky76@users.noreply.github.com>
2022-09-18 14:27:51 +0530

    Add files via upload

---
+++ Variables_Demo.java
@@ -0,0 +1,22 @@
+package java_prac;
+
+public class Variables_Demo {
+
+	public static void main(String[] args)
+	{
+		
+		String empno ="E001";
+		String empname ="Richards";
+		byte age =24;
+		char gender ='M';
+		int salary =40000;
+		boolean working =true;
+		
+		System.out.println(empno);
+		System.out.println("Employee Name : "+empname);
+		System.out.println("Employee Age :"+age);
+		System.out.println(salary+age);
+
+	}
+
+}
