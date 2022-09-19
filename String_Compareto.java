ffde91b3d5d70a8a3ba7286a630306e4d43af136
Deviky76 <111038886+Deviky76@users.noreply.github.com>
2022-09-19 13:37:34 +0530

    Add files via upload

---
+++ String_Compareto.java
@@ -0,0 +1,15 @@
+package java_prac;
+
+public class String_Compareto {
+
+	public static void main(String[] args) 
+	{
+	
+		String str1, str2;
+		str1 = "A";
+		str2 = "z";
+		System.out.println(str2.compareTo(str1));
+
+	}
+
+}
