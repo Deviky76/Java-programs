3a086432a7da1244326b1a42500b59ab0651de99
Deviky76 <111038886+Deviky76@users.noreply.github.com>
2022-09-19 16:25:05 +0530

    Add files via upload

---
+++ Arry_Object.java
@@ -0,0 +1,18 @@
+package java_prac;
+
+public class Arry_Object {
+
+	public static void main(String[] args) 
+	{
+	
+		Object[] empdata ={"Richards", 23, 'm',40000.50, true};
+		
+		System.out.println(empdata[0]);
+		System.out.println(empdata[1]);
+		System.out.println(empdata[2]);
+		System.out.println(empdata[3]);
+		
+
+	}
+
+}
