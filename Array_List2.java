65e30a8402e2141ee1ad21766be0b374e251d329
Deviky76 <111038886+Deviky76@users.noreply.github.com>
2022-09-19 16:21:16 +0530

    Add files via upload

---
+++ Array_List2.java
@@ -0,0 +1,19 @@
+package java_prac;
+
+public class Array_List2 {
+
+	public static void main(String[] args) 
+	
+	{
+		int marks[] = {50,60,70,80};
+		
+		System.out.println(marks.length);
+		
+		System.out.println(marks[0]);
+		System.out.println(marks[1]);
+		System.out.println(marks[2]);
+		System.out.println(marks[3]);
+		
+	}
+
+}
