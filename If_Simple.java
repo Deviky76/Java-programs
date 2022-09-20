30eb5b0ba847ae71013afe694de6abd98792b0ce
Deviky76 <111038886+Deviky76@users.noreply.github.com>
2022-09-20 19:34:00 +0530

    Add files via upload

---
+++ If_Simple.java
@@ -0,0 +1,23 @@
+package java_prac;
+
+public class If_Simple {
+
+	public static void main(String[] args) 
+	{
+	
+		int a, b;
+		a=10;
+		b=20;
+		if(a>b)
+		{
+			System.out.println("a is big");
+			
+		}
+		else
+		{
+			System.out.println("b is big");
+		}
+
+	}
+
+}
