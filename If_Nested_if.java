7829c06de28d6ec012be0206ca51c627cdcfa23c
Deviky76 <111038886+Deviky76@users.noreply.github.com>
2022-09-20 20:04:54 +0530

    Add files via upload

---
+++ If_Nested_if.java
@@ -0,0 +1,27 @@
+package java_prac;
+
+public class If_Nested_if {
+
+	public static void main(String[] args) 
+	{
+	
+		int a,b;
+		a =10;
+		b =10;
+		if(a>b)
+		{
+			System.out.println("a is big");
+		}
+		else if(b>a)
+		{
+			System.out.println("b is big");
+			
+		}
+		else
+		{
+			System.out.println("both are same");
+		}
+
+	}
+
+}
