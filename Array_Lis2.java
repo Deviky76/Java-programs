a5ef8262b8b3b6ba78b5559454f4ae6528505e6f
Deviky76 <111038886+Deviky76@users.noreply.github.com>
2022-09-20 18:46:09 +0530

    Add files via upload

---
+++ Array_Lis2.java
@@ -0,0 +1,23 @@
+package java_prac;
+
+import java.util.ArrayList;
+
+public class Array_Lis2 {
+
+	public static void main(String[] args) 
+	{
+	
+		ArrayList<String> carlist = new ArrayList<>();
+				
+		//Adding Elements into an Array using : add()
+		carlist.add("BMW");
+		carlist.add("Audi");
+		carlist.add("Volva");
+		carlist.add("Honda");
+		
+		System.out.println(carlist.contains("Audi"));
+		
+
+	}
+
+}
