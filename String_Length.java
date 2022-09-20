2b82c60feb93404303385f82e89c467bfe710a50
Deviky76 <111038886+Deviky76@users.noreply.github.com>
2022-09-20 19:21:51 +0530

    Add files via upload

---
+++ String_Length.java
@@ -0,0 +1,21 @@
+package java_prac;
+
+public class String_Length {
+
+	public static void main(String[] args) 
+	{
+	
+		String str = "Rabort";
+		String str1 = "Google";
+		System.out.println(str.length());
+		 
+		//Find the last character of the given string
+		
+		System.out.println(str.charAt(0));
+		
+		System.out.println(str1.charAt(str1.length()-1));
+		
+
+	}
+
+}
