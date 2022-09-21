a8ba67536df14c4b98a09e0f4e0176d8bb4f504e
Deviky76 <111038886+Deviky76@users.noreply.github.com>
2022-09-21 14:52:42 +0530

    Add files via upload

---
+++ For_Loop1.java
@@ -0,0 +1,19 @@
+package java_prac;
+
+public class For_Loop1 {
+
+	public static void main(String[] args) 
+	{
+	
+		int[] marks = {60,70,80,90,100};
+		
+		
+		for(int i=0; i<marks.length ; i++)
+		{
+			int data = marks[i]; 
+			System.out.println(data);
+		}
+
+	}
+
+}
