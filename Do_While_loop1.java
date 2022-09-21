17096573665f87b587ccf494c8591f398fb00cd0
Deviky76 <111038886+Deviky76@users.noreply.github.com>
2022-09-21 12:16:13 +0530

    Add files via upload

---
+++ Do_While_loop1.java
@@ -0,0 +1,20 @@
+package java_prac;
+
+public class Do_While_loop1 {
+
+	public static void main(String[] args) 
+	{
+	
+		int i=98;
+		
+		do
+		{
+			System.out.println(i);
+			
+			i=i+1;
+		}
+		while (i<=10);	
+		
+	}
+
+}
