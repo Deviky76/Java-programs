cfc8d801c22ce43f6797256cbfdf5ff32c66b73f
Deviky76 <111038886+Deviky76@users.noreply.github.com>
2022-09-21 12:06:30 +0530

    Add files via upload

---
+++ Whil_Loop1.java
@@ -0,0 +1,20 @@
+package java_prac;
+
+public class Whil_Loop1 {
+
+	public static void main(String[] args) 
+	{
+	
+		int i =1;
+		
+		while(i<=10)
+		{
+			System.out.println(i);
+			i++;
+			
+		}
+
+		System.out.println("End of the While Loop");
+	}
+
+}
